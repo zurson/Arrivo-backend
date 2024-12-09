6da7c105ba333a231618e58e58608971741ff8fd
zurson <matu.jurek@gmail.com>
2024-12-09 13:11:39 +0100

    Project setup

---
+++ src/main/kotlin/com/arrivo/ArrivoApplication.kt
@@ -0,0 +1,11 @@
+package com.arrivo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArrivoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArrivoApplication>(*args)
+}
---
+++ src/test/kotlin/com/arrivo/ArrivoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arrivo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArrivoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
