6d0a1a198219c18f34ba92b9570e94747a60b09d
zurson <matu.jurek@gmail.com>
2025-01-14 19:39:11 +0100

    Added TaskDTO and endpoint for getting free tasks

---
+++ src/main/kotlin/com/arrivo/task/TaskDTO.kt
@@ -0,0 +1,17 @@
+package com.arrivo.task
+
+import com.arrivo.employee.Employee
+import com.arrivo.task.products.Product
+import com.arrivo.utilities.Location
+import java.time.LocalDateTime
+
+data class TaskDTO(
+    val id: Long,
+    var title: String,
+    var location: Location,
+    var addressText: String,
+    var status: TaskStatus,
+    var assignedDate: LocalDateTime?,
+    var employee: Employee?,
+    val products: List<Product> = listOf()
+)
